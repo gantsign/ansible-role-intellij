f45b877de932b6d4d6b75fdc94f27b141133ba95
John Freeman <john.freeman.opensource@gmail.com>
2016-09-05 23:24:38 +0100

    Upgraded to Molecule 1.10.3
    Fixed flake8 linter and ansible-lint warnings.

--- tests/test_role.py
+++ tests/test_role.py
@@ -8,11 +8,14 @@ testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 def test_idea_installed(Command):
     assert Command('which idea').rc == 0
 
+
 @pytest.mark.parametrize('file_path,expected_text', [
     ('disabled_plugins.txt', 'org.jetbrains.plugins.gradle'),
     ('options/jdk.table.xml', '/usr/lib/jvm/java-1.8.0-openjdk-amd64'),
     ('options/project.default.xml', '/test/maven/home')
 ])
 def test_config_files(Command, File, file_path, expected_text):
-    config_home = Command.check_output('find /home/test_usr | grep --color=never -E %s', '\\.IdeaIC[0-9]+\\.[0-9]/config$')
+    config_home = Command.check_output('find %s | grep --color=never -E %s',
+                                       '/home/test_usr',
+                                       '\\.IdeaIC[0-9]+\\.[0-9]/config$')
     assert File(config_home + '/' + file_path).contains(expected_text)
