89a4ac9bb55e54cfe1a26a5a87d4e74daecce6db
John Freeman <john.freeman.opensource@gmail.com>
2018-04-22 21:30:19 +0100

    Made default Maven installation var optional (#149)
    To make the role easier to use.
    
    Enhancement: resolves #118

--- molecule/minimal/tests/test_role.py
+++ molecule/minimal/tests/test_role.py
@@ -8,14 +8,3 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 def test_idea_installed(Command):
     assert Command('which idea').rc == 0
-
-
-def test_config_files(Command, File):
-    file_path = 'options/project.default.xml'
-    expected_text = '/test/maven/home'
-
-    config_dir_pattern = '\\.(IdeaIC|IntelliJIdea)[0-9]+\\.[0-9]/config$'
-    config_home = Command.check_output('find %s | grep --color=never -E %s',
-                                       '/home/test_usr',
-                                       config_dir_pattern)
-    assert File(config_home + '/' + file_path).contains(expected_text)
