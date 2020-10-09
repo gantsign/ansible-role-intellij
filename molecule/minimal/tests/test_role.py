f91a3e733fdde6806a1eb9795e6dcebfd7261b7f
John Freeman <john.freeman.opensource@gmail.com>
2020-10-09 16:25:27 +0100

    Updated Molecule to 3.0.8 (#270)
    Keeping up with the latest changes.

--- molecule/minimal/tests/test_role.py
+++ molecule/minimal/tests/test_role.py
@@ -1,11 +1,2 @@
-import os
-
-import testinfra.utils.ansible_runner
-
-
-testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
-
-
 def test_idea_installed(host):
     assert host.run('which idea').rc == 0
